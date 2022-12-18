6165a2c25e614041d646d43b7eef5a38b371f782
Osama Muhamed <63379624+U-sama@users.noreply.github.com>
2022-12-18 16:23:59 +0200

    db relations

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Author, Address, Country
 # Register your models here.
 
 class BookAdmin(admin.ModelAdmin):
@@ -8,4 +8,13 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ("author", "rating",) # makng a filter on the right of the page to filter the date by the values of each column
     list_display = ("title", "author",) # which columns to display 
 
+class AuthorAdmin(admin.ModelAdmin):
+  list_display = ("first_name", "last_name",)
+
+# class AddressAdmin(admin.ModelAdmin):
+#   list_display = ()
+
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Address)
+admin.site.register(Country)
